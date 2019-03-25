aa73033801699e576c8a2cb38024fc876e3fca95
Marius <marius.guerard@gmail.com>
2019-03-25 14:37:01 -0700

    Implemented DBSCAN Clusterization, based on density (should be similar to SPC)

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ Author: Marius Guerard
 """
 
 ### For debuging.
-import matplotlib.pyplot as plt
-import pywt
-import numpy as np
-import sklearn as sk
-from scipy import stats
 
 import helper_function as hf
 import feature_extraction as fe
+import visualization as vis
 
 ##############
 # PARAMETERS #
@@ -54,33 +50,46 @@ def spike_sort(data_path=DATA_PATH, n_clus=N_CLUS, t_min_max=T_MIN_MAX):
 
 
     ### Extract features from spikes and cluster them.
-    features_1 = fe.feat_clust(wave_form, feat_func=fe.dwt_multimodal)
+    features_1 = fe.feat_clust(wave_form, feat_func=fe.dwt_multimodal,
+                               clust_type=fe.dbscan)
 
     ### Plot the cluster map and their average wave form.
-    hf.plot_features_cluster(wave_form, sf, features_1)
+    vis.plot_features_cluster(wave_form, sf, features_1, dim=3)
 
     ### Plot the raw signal and the filtered signal between time
     ### 'T_MIN_MAX[0]' and 'T_MIN_MAX[1]'.
-    # hf.plot_amplitude(time_vec, amp, sf, t_min=t_min_max[0], t_max=t_min_max[1])
-    # hf.plot_amplitude(time_vec, filtered_amp, sf, t_min=t_min_max[0],
+    # vis.plot_amplitude(time_vec, amp, sf, t_min=t_min_max[0], t_max=t_min_max[1])
+    # vis.plot_amplitude(time_vec, filtered_amp, sf, t_min=t_min_max[0],
     #                   t_max=t_min_max[1])
     
     ### Plot the spikes maximum positions.
     # plt.vlines(time[pos], -200, 100)
     ### Plot a random subset of the spikes wave_form
-    # hf.plot_random_spikes(wave_form)
+    # vis.plot_random_spikes(wave_form)
  
     
     return raw, pos, wave_form, features_1
 
 if __name__ == "__main__":
+
     raw, pos, wave_form, features_1 = spike_sort()
 
 
-
-    # coeffs_concat = np.concatenate(pywt.wavedec(wave_form, 'haar', level=4),
-    #                                axis=1)
-
-    # coeff_reduced = fe.dwt_multimodal(coeffs_concat)
-
+    # import matplotlib.pyplot as plt
+    # import pywt
+    # import numpy as np
+    # import sklearn as sk
+    # from sklearn.cluster import DBSCAN
+    # from sklearn.preprocessing import StandardScaler
+    # from scipy import stats
     
+
+    # coeff_reduced = fe.dwt_multimodal(wave_form)
+
+
+    # dbscan_list = []
+    # eps_range = np.linspace(1.2, 1.7, 10)
+    # for eps_i in eps_range:
+    #     dbscan_list.append(fe.dbscan(coeff_reduced, eps=eps_i))
+        
+
